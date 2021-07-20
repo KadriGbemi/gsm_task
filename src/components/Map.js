132ac54e00e2022d5372cb2e5b0dfcf9a057b5f9
“KadriGbemi” <kadri.oluwagbemi@gmail.com>
2021-07-20 15:42:46 +0300

    Add default location of the marker

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,23 +10,22 @@ const containerStyle = {
 
 const center = { lat: 59.437, lng: 24.7536 };
 function Map({ address }) {
-  const [point, setPoint] = useState({ lat: 59.437, lng: 24.7536 });
+  const [point, setPoint] = useState({ lat: 59.440263, lng: 24.730345 });
   useEffect(() => {
-    // console.log("Map address", address);
+    console.log("Map address", address);
     if (address && address.coordinates && address.coordinates.length) {
-      const [long, latitude ] = address.coordinates;
-
+      const [long, latitude] = address.coordinates;
+      console.log("Map address cord", long, latitude);
       setPoint({
-        lat: latitude || 44.96323,
-        lng: long || 11.62537,
+        lat: latitude || 59.440263,
+        lng: long || 24.730345,
       });
     }
   }, [address]);
   return (
-    console.log("Center", point),
     (
       <LoadScript googleMapsApiKey="AIzaSyA8gA7OUb996h1q7XN3SFXT9LVY-uHC4EU">
-        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={20}>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={13}>
           <Marker position={point} />
         </GoogleMap>
       </LoadScript>
