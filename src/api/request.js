1ef805c049f0d559cf35c96ec655a2d44992e516
“KadriGbemi” <kadri.oluwagbemi@gmail.com>
2021-07-20 13:50:15 +0300

    Handle create task form implementation

---
+++ src/api/request.js
@@ -0,0 +1,47 @@
+const requestData = async (url, method, address) => {
+  const data = {
+    account:
+      "https://gsmtasks.com/api/tasks/accounts/cb57aa97-e4e3-430b-84b4-a93d0b8bdf7a/",
+    address: {
+      raw_address: address,
+    },
+    category: "assignment",
+  };
+  const options = {
+    method: method,
+    mode: "cors",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Token c8bd28ffc939c3107838301fff3cc81269434722",
+    },
+  };
+  const response = await fetch(
+    url,
+    method === "GET"
+      ? options
+      : {
+          ...options,
+          body: JSON.stringify(data),
+        }
+  );
+  return response;
+};
+export const handleCreateTask = async (address) => {
+  const response = await requestData(
+    "https://gsmtasks.com/api/tasks/tasks/",
+    "POST",
+    address
+  );
+  if (response.ok) {
+    let data = await response.json();
+    if (data && data.url) {
+      const location = await requestData(data.url, "GET");
+
+      if (response.ok) {
+        let locationData = await location.json();
+        console.log("Response location", locationData);
+      }
+    }
+  }
+};
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,25 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { handleCreateTask } from "../api/request";
 
 const handleSubmit = (e) => {
   e.preventDefault();
-  const formData = new FormData(e.target)
-  const address = formData.get('address')
+  const formData = new FormData(e.target);
+  const address = formData.get("address");
   console.log("Address is submitted", address);
+  if (address) {
+    handleCreateTask(address);
+  }
 };
 function MapForm() {
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formAddress">
         <Form.Label>Address</Form.Label>
-        <Form.Control type="text" placeholder="Enter address" name="address"/>
+        <Form.Control type="text" placeholder="Enter address" name="address" />
       </Form.Group>
       <Button variant="primary" type="submit">
-        Save
+        Save a task
       </Button>
     </Form>
   );
